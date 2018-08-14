1cd6527731440081e4f52b985ca9472d271c30ed
Nita <Nita@DESKTOP-PHVQM3J>
2018-08-14 12:42:57 +0700

    Initial Commit

---
+++ src/LATIHAN/CobaOperasiString.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LATIHAN;
+
+/**
+ *
+ * @author Nita
+ */
+public class CobaOperasiString {
+    public static void main (String [] args ) {
+        
+        String identitas = "DEAR REGITA PERMATASARI / X RPL 5 / 11 ";
+                System.out.println("Identitas : " + identitas);
+    }
+    
+}
