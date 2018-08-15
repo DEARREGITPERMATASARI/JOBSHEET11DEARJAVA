b046535d38869e2efefdc79742d41f52ae5cf0ba
Nita <Nita@DESKTOP-PHVQM3J>
2018-08-15 08:23:20 +0700

    Initial Commit

---
+++ src/LATIHAN/ConvertVocal2Angka.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LATIHAN;
+
+/**
+ *
+ * @author Nita
+ */
+public class ConvertVocal2Angka {
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : " +identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka) \n ");
+        
+    }
+     public static void main (String [] args) {
+    
+String identitas = "DEAR REGITA PERMATASARI / X RPL 5 / 11";
+    tampilJudul(identitas);
+    
+    
+}
+}
